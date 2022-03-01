60d9757c6e3562950ff952ca6a0d66f3fd6d9fda
Thiago Bertolino <thiagopbertolino@gmail.com>
2022-03-01 13:56:10 -0300

    Commit

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
     }
 }
 
